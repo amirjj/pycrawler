e7ea3496131c4085554c898207154622d02e73d1
amir jamshidi <amir.jamshidijam@gmail.com>
2016-11-12 14:58:19 +0330

    used dictionary for url handler to  be uniq

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import requests
 import re
+from collections import defaultdict
+
 try:
     from urllib.parse import urljoin
 except ImportError:
@@ -15,29 +17,30 @@ def outputhandler(newurl):
     urloutputfile.close()
 
 def crawl(url):
+    try:
+        req = requests.get(url)
 
-    req = requests.get(url)
+        # Check if successful
+        if(req.status_code != 200):
+            return []
 
-    # Check if successful
-    if(req.status_code != 200):
-        return []
+        # Find links
+        links = link_re.findall(req.text)
 
-    # Find links
-    links = link_re.findall(req.text)
-
-    print("\nFound {} links".format(len(links)))
-    urls_fetched = list()
-    # Search links for emails
-    for link in links:
-        # Get an absolute URL for a link
-        link = urljoin(url, link)
-        urls_fetched.append(str(link))
-        # print link
-    return urls_fetched
+        print("\nFound {} links".format(len(links)))
+        urls_fetched = defaultdict(int)
+        # Search links for emails
+        for link in links:
+            # Get an absolute URL for a link
+            link = urljoin(url, link)
+            urls_fetched[str(link)] = 0
+        return urls_fetched
+    except:
+        print url
 
 if __name__ == '__main__':
     urlfile = open('urls.txt',"r")
     urlbase = list()
     for url in urlfile:
         urlbase = crawl(url)
-    outputhandler(urlbase)
+        outputhandler(urlbase)
